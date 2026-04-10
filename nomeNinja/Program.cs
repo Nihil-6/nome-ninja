010e3f0d3e22378425f9d406889d1d835ea8c01a
Davi <davi.xa.oliveira@gmail.com>
2026-04-10 20:45:11 -0300

    exibe seu nome ninja

---
+++ nomeNinja/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Qual é seu nome?");
+String nmdUsu = Console.ReadLine()!;
+
+String nmninja = nmdUsu
+ .Replace("a", "KA")
+ .Replace("b", "ZU")
+ .Replace("c", "MI")
+ .Replace("d", "TE")
+ .Replace("e", "KU")
+ .Replace("f", "LU")
+ .Replace("g", "JI")
+ .Replace("h", "RI")
+ .Replace("i", "KI")
+ .Replace("j", "ZUS")
+ .Replace("k", "ME")
+ .Replace("l", "TA")
+ .Replace("m", "RIM")
+ .Replace("n", "TO")
+ .Replace("o", "MO")
+ .Replace("p", "NO")
+ .Replace("q", "KE")
+ .Replace("r", "SHI")
+ .Replace("s", "ARI")
+ .Replace("t", "CHI")
+ .Replace("u", "DO")
+ .Replace("v", "RU")s
+ .Replace("w", "MEI")
+ .Replace("x", "NA")
+ .Replace("y", "FU")
+ .Replace("z", "ZI");
+
+Console.WriteLine(nmninja);
